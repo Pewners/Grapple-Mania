1ca16705813f0d45472a0fec6a219c87a3e12a3a
Pewners <jfadikpe2021@elam.co.uk>
2023-03-14 22:13:20 +0000

    shooty

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class Shooting : MonoBehaviour
 {
     public GameObject bullet;
 
