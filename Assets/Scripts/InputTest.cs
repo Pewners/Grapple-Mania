3819126baa6fd0f898e189185fac1f0edcd6c903
Pewners <jfadikpe2021@elam.co.uk>
2023-04-11 21:43:52 +0100

    new input system
    testing jump

---
+++ Assets/Scripts/InputTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTest : MonoBehaviour
+{
+    public void Jump()
+    {
+        Debug.Log("Jump");
+    }
+}
